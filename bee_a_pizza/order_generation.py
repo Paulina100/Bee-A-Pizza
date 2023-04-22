0f71b6edae26c20e77370bd638c2498f24caf995
pietruszkowiec <pietruszkowiec@gmail.com>
2023-04-22 17:57:24 +0200

    Reformat with black

--- bee_a_pizza/order_generation.py
+++ bee_a_pizza/order_generation.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 
-def generate_n_slices_per_customer(min_slices: int, max_slices: int, n_customers: int) -> np.ndarray:
+def generate_n_slices_per_customer(
+    min_slices: int, max_slices: int, n_customers: int
+) -> np.ndarray:
     """
     Randomly generates number of pizza slices desired by every customer.
 
@@ -15,10 +17,14 @@ def generate_n_slices_per_customer(min_slices: int, max_slices: int, n_customers
     -------
     `n_slices_per_customer` : (n_customers)
     """
-    return np.random.randint(low=min_slices, high=max_slices, size=n_customers, dtype='int8')
+    return np.random.randint(
+        low=min_slices, high=max_slices, size=n_customers, dtype="int8"
+    )
 
 
-def generate_preferences(n_customers: int, n_ingredients, avg_likes: int, avg_dislikes: int) -> np.ndarray:
+def generate_preferences(
+    n_customers: int, n_ingredients, avg_likes: int, avg_dislikes: int
+) -> np.ndarray:
     """
     Randomly generates the ingredient preferences of customers.
     1 for liked ingredients
@@ -36,10 +42,17 @@ def generate_preferences(n_customers: int, n_ingredients, avg_likes: int, avg_di
     -------
     `preferences` : (n_customers, n_ingredients)
     """
-    if n_ingredients <= 0 or avg_likes < 0 or avg_dislikes < 0 or n_ingredients < avg_likes + avg_dislikes:
+    if (
+        n_ingredients <= 0
+        or avg_likes < 0
+        or avg_dislikes < 0
+        or n_ingredients < avg_likes + avg_dislikes
+    ):
         raise ValueError
 
-    distribution = np.random.randint(low=0, high=n_ingredients, size=(n_customers, n_ingredients), dtype='int32')
+    distribution = np.random.randint(
+        low=0, high=n_ingredients, size=(n_customers, n_ingredients), dtype="int32"
+    )
     preferences = np.where(distribution < avg_dislikes, -1, 0)
     preferences = np.where(distribution >= n_ingredients - avg_likes, 1, preferences)
 
@@ -59,7 +72,7 @@ def assign_customers_to_slices(n_slices_per_customer: np.ndarray) -> np.ndarray:
     `slice_assignment` : (n_slices_total)
     """
     n_slices_total = np.sum(n_slices_per_customer)
-    slice_assignment = np.zeros(n_slices_total, dtype='uint8')
+    slice_assignment = np.zeros(n_slices_total, dtype="uint8")
     first_slice = 0
     last_slice = 0
     for customer, n_slices in enumerate(n_slices_per_customer):
@@ -70,7 +83,9 @@ def assign_customers_to_slices(n_slices_per_customer: np.ndarray) -> np.ndarray:
     return slice_assignment
 
 
-def get_preferences_by_slice(preferences: np.ndarray, n_slices_per_customer: np.ndarray) -> np.ndarray:
+def get_preferences_by_slice(
+    preferences: np.ndarray, n_slices_per_customer: np.ndarray
+) -> np.ndarray:
     """
     Assigns customers' ingredient preferences to their assigned slices
 
@@ -83,5 +98,7 @@ def get_preferences_by_slice(preferences: np.ndarray, n_slices_per_customer: np.
     -------
     `preferences_by_slice` : (n_slices_total, n_ingredients)
     """
-    slice_assignment = assign_customers_to_slices(n_slices_per_customer=n_slices_per_customer)
+    slice_assignment = assign_customers_to_slices(
+        n_slices_per_customer=n_slices_per_customer
+    )
     return preferences[slice_assignment]
--- tests/test_order_generation.py
+++ tests/test_order_generation.py
@@ -4,9 +4,7 @@ from bee_a_pizza.order_generation import *
 
 def test_generate_n_slices_per_customer():
     n_slices_per_customer = generate_n_slices_per_customer(
-        min_slices=2,
-        max_slices=5,
-        n_customers=7
+        min_slices=2, max_slices=5, n_customers=7
     )
     assert np.all((n_slices_per_customer >= 2) & (n_slices_per_customer <= 5))
 
@@ -14,25 +12,16 @@ def test_generate_n_slices_per_customer():
 def test_generate_preferences():
     with pytest.raises(ValueError):
         generate_preferences(
-            n_customers=5,
-            n_ingredients=0,
-            avg_likes=0,
-            avg_dislikes=0
+            n_customers=5, n_ingredients=0, avg_likes=0, avg_dislikes=0
         )
 
     with pytest.raises(ValueError):
         generate_preferences(
-            n_customers=5,
-            n_ingredients=5,
-            avg_likes=3,
-            avg_dislikes=3
+            n_customers=5, n_ingredients=5, avg_likes=3, avg_dislikes=3
         )
 
     preferences = generate_preferences(
-        n_customers=5,
-        n_ingredients=15,
-        avg_likes=3,
-        avg_dislikes=3
+        n_customers=5, n_ingredients=15, avg_likes=3, avg_dislikes=3
     )
 
     assert np.all((preferences >= -1) & (preferences <= 1))
@@ -48,23 +37,13 @@ def test_assign_customers_to_slices():
 
 
 def test_get_preferences_by_slice():
-    preferences = np.array(
-        [[1, 1, -1],
-         [0, 0, 1],
-         [-1, 0, -1]]
-    )
+    preferences = np.array([[1, 1, -1], [0, 0, 1], [-1, 0, -1]])
     n_slices_per_customer = np.array([2, 3, 1])
     expected_preferences_by_slice = np.array(
-        [[1, 1, -1],
-         [1, 1, -1],
-         [0, 0, 1],
-         [0, 0, 1],
-         [0, 0, 1],
-         [-1, 0, -1]]
+        [[1, 1, -1], [1, 1, -1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [-1, 0, -1]]
     )
     preferences_by_slice = get_preferences_by_slice(
-        preferences=preferences,
-        n_slices_per_customer=n_slices_per_customer
+        preferences=preferences, n_slices_per_customer=n_slices_per_customer
     )
 
     assert np.all(preferences_by_slice == expected_preferences_by_slice)
