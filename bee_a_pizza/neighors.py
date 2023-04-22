5cca5c4a9ad904d33e0e8334f3c8acaa53cf2b93
Oskar Simon <oskarsimon3@gmail.com>
2023-04-22 19:35:11 +0200

    Added docstring to get_neighbor().

--- bee_a_pizza/neighors.py
+++ bee_a_pizza/neighors.py
@@ -10,7 +10,16 @@ def swap_slices(results: np.ndarray, n_slices_to_swap: int) -> np.ndarray:
 
 
 def get_neighbor(results: np.ndarray, search_cycle_proportion: float, pizza_swap_proba: float = 0.5) -> np.ndarray:
+    """
+    Returns a neighbor of the given solution generated either by swap_pizzas() or swap_slices(), chosen randomly
 
+    Parameters
+    ----------
+    `results` : (n_slices, n_pizzas)
+    `search_cycle_proportion` : float (0 - 1) - decides how distant the neighbor is, relative to the current solution
+        (i.e. 1 means all slices/pizzas need to be swapped, 0.5 means only 50% of them needs to be swapped)
+    `pizza_swap_proba` : probability of choosing swap_pizzas() over swap_slices()
+    """
     if np.random.rand(1) < pizza_swap_proba:
         n_pizzas_to_change = results.shape[0] * search_cycle_proportion // 1
         return swap_pizzas(results, n_pizzas_to_change)
